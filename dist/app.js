7db8a0e78fe1fc259fe0476f36eb68cb711757c4
eriveltondasilva <eriveltondasilva13@gmail.com>
2023-05-04 21:57:38 -0300

    .

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,13 @@
 "use strict";
-// # const
+var _a;
 const KEY_NUMBERS = document.querySelectorAll(".js-number");
 const OPERATION = document.querySelector(".js-operation");
 const RESULT = document.querySelector(".js-result");
-//
+let test = (_a = RESULT.textContent) === null || _a === void 0 ? void 0 : _a.length;
+OPERATION.textContent = toString(test);
 KEY_NUMBERS.forEach((number) => number.addEventListener("click", typeNumber));
 function typeNumber() {
     let value = this.value;
-    OPERATION.innerHTML += value;
+    RESULT.textContent += value;
 }
+OPERATION.textContent = "";
