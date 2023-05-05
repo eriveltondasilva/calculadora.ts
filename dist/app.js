2a0e75ea4ce92ed6af9fba9ec9e18e4a91e01fc3
eriveltondasilva <eriveltondasilva13@gmail.com>
2023-05-05 04:00:45 -0300

    funcionando teclas numéricas

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,55 @@
 "use strict";
-var _a;
-const KEY_NUMBERS = document.querySelectorAll(".js-number");
-const OPERATION = document.querySelector(".js-operation");
-const RESULT = document.querySelector(".js-result");
-let test = (_a = RESULT.textContent) === null || _a === void 0 ? void 0 : _a.length;
-OPERATION.textContent = toString(test);
+const SELECTOR = (selector) => document.querySelector(selector);
+const SELECTOR_BTN = (selector) => document.querySelectorAll(selector);
+const KEY_NUMBERS = SELECTOR_BTN(".js-number");
+const KEY_OPERATORS = SELECTOR_BTN(".js-operator");
+const OPERATION = SELECTOR(".js-operation");
+const RESULT = SELECTOR(".js-result");
+const DELETE = SELECTOR(".js-delete");
+const DELETE_ALL = SELECTOR(".js-delete-all");
+const displayLimiter = 8;
+DELETE_ALL.addEventListener("click", () => {
+    RESULT.textContent = "";
+    OPERATION.textContent = "";
+});
 KEY_NUMBERS.forEach((number) => number.addEventListener("click", typeNumber));
 function typeNumber() {
-    let value = this.value;
+    var _a, _b;
+    const value = this.value;
+    const displayLength = (_b = (_a = RESULT.textContent) === null || _a === void 0 ? void 0 : _a.length) !== null && _b !== void 0 ? _b : 0;
+    _displayLength(displayLength);
+    _limiterZero(value, displayLength);
+    _limiterDot(value);
     RESULT.textContent += value;
 }
-OPERATION.textContent = "";
+function _displayLength(displayLength) {
+    if (displayLength >= displayLimiter) {
+        throw "";
+    }
+}
+function _limiterZero(value, displayLength) {
+    var _a;
+    const firstCharacter = (_a = RESULT.textContent) === null || _a === void 0 ? void 0 : _a.charAt(0);
+    if (firstCharacter === "0" && value === ",") {
+        return;
+    }
+    if (firstCharacter === "0" && displayLength === 1) {
+        RESULT.textContent = "";
+        return;
+    }
+    if (firstCharacter === "0" && value === "0") {
+        throw "";
+    }
+}
+function _limiterDot(value) {
+    var _a;
+    const DOT = (_a = RESULT.textContent) === null || _a === void 0 ? void 0 : _a.includes(",");
+    if (DOT && value === ",") {
+        throw "";
+    }
+}
+KEY_OPERATORS.forEach((operator) => operator.addEventListener("click", typeOperator));
+function typeOperator() {
+    const value = this.value;
+    RESULT.textContent += value;
+}
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,82 @@
-// *const
-const KEY_NUMBERS = document.querySelectorAll<HTMLButtonElement>(".js-number");
-const OPERATION = document.querySelector(".js-operation") as HTMLElement;
-const RESULT = document.querySelector(".js-result") as HTMLElement;
+// * shortcuts
+const SELECTOR = (selector: string) => document.querySelector<HTMLElement>(selector)!;
+const SELECTOR_BTN = (selector: string) => document.querySelectorAll<HTMLButtonElement>(selector)!;
 
-// *let
-let test = RESULT.textContent?.length;
+// * const para capturar elementos html
+const KEY_NUMBERS = SELECTOR_BTN(".js-number");
+const KEY_OPERATORS = SELECTOR_BTN(".js-operator");
 
-OPERATION.textContent = toString(test);
+const OPERATION = SELECTOR(".js-operation");
+const RESULT = SELECTOR(".js-result");
+const DELETE = SELECTOR(".js-delete");
+const DELETE_ALL = SELECTOR(".js-delete-all");
 
+// * const
+const displayLimiter = 8;
 
-//
+// * funções
+
+// Deleta todas as informações da tela.
+DELETE_ALL.addEventListener("click", () => {
+    RESULT.textContent = "";
+    OPERATION.textContent = "";
+});
+
 KEY_NUMBERS.forEach((number) => number.addEventListener("click", typeNumber));
 
-function typeNumber(this: HTMLButtonElement) {
-    let value = this.value;
+function typeNumber(this: HTMLButtonElement): void {
+    const value = this.value;
+    const displayLength = RESULT.textContent?.length ?? 0;
 
-    // if (condition) {
+    _displayLength(displayLength);
 
-    // }
+    _limiterZero(value, displayLength);
+
+    _limiterDot(value);
 
     RESULT.textContent += value;
 }
 
-OPERATION.textContent = "";
+// funções auxiliares
+function _displayLength(displayLength: number): void {
+    if (displayLength >= displayLimiter) {
+        throw "";
+    }
+}
+
+//
+function _limiterZero(value: string, displayLength: number): void {
+    const firstCharacter = RESULT.textContent?.charAt(0);
+
+    if (firstCharacter === "0" && value === ",") {
+        return;
+    }
+
+    if (firstCharacter === "0" && displayLength === 1) {
+        RESULT.textContent = "";
+        return;
+    }
+
+    if (firstCharacter === "0" && value === "0") {
+        throw "";
+    }
+}
+
+//
+function _limiterDot(value: string) {
+    const DOT = RESULT.textContent?.includes(",");
+
+    if (DOT && value === ",") {
+        throw "";
+    }
+}
+
+//
+
+KEY_OPERATORS.forEach((operator) => operator.addEventListener("click", typeOperator));
+
+function typeOperator(this: HTMLButtonElement): void {
+    const value = this.value;
+
+    RESULT.textContent += value;
+}
